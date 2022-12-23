3cebb76ead7626948e8f9ed9f37cc594fe60cf8c
ramvilsil <115331883+ramvilsil@users.noreply.github.com>
2022-12-23 11:55:23 -0800

    fixed buttons
    - added icon to message button
    - fixed button heights
    - fixed button icon positionings

--- pages/projects/theatre.tsx
+++ pages/projects/theatre.tsx
@@ -52,7 +52,7 @@ const Theatre = () => {
 
                             </div>
 
-                            <a href='https://ramvilsil.github.io/summary-theatrecms' className="bg-gradient-to-br from-black via-neutral-900 to-neutral-900 w-max p-2 m-4 rounded-xl hover:opacity-60 text-lg flex">Read More &nbsp;<svg className="translate-y-1" xmlns="http://www.w3.org/2000/svg" height={20} fill="white" viewBox="0 0 600 512"><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z" /></svg></a>
+                            <a href='https://ramvilsil.github.io/summary-theatrecms' className="bg-gradient-to-br from-black via-neutral-900 to-neutral-900 w-18 h-10 p-2 m-4 rounded-xl hover:opacity-60 text-lg flex">Read More &nbsp; <svg className="translate-y-0.5" xmlns="http://www.w3.org/2000/svg" height={20} fill="white" viewBox="0 0 600 512"><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z" /></svg></a>
 
                         </div>
 
