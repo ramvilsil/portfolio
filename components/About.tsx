412d5ff2c012e141620a45f754d90919b262ee07
ramvilsil <115331883+ramvilsil@users.noreply.github.com>
2022-12-28 11:17:35 -0800

    updated about text

--- components/About.tsx
+++ components/About.tsx
@@ -9,18 +9,23 @@ const About = () => {
             <div className="bg-gradient-to-br from-zinc-700 via-zinc-800 to-zinc-700 text-white rounded-3xl p-8 portrait:p-4 shadow-lg shadow-black">
 
                 {/* About Title */}
-                <h1 className="w-max text-3xl m-4 font-semibold portrait:text-2xl">About<div className="text-lg font-normal portrait:hidden">What I'm up to.</div></h1>
+                <h1 className="w-max text-3xl m-4 font-semibold portrait:text-2xl">About<div className="text-lg font-normal portrait:hidden">What I do.</div></h1>
 
                 {/* About Text */}
                 <div className="p-4 font-light">
 
-                    I've always enjoyed programming as a whole, not just web development.
-                    Prior to getting into web development, I had knowledge of Python and JavaScript programming from high school.
-                    In the summer of 2022, I began self-teaching, and in the fall, I enrolled in a web development bootcamp.
+                    I'm a dedicated developer with full-stack expertise.
 
-                    <br></br> <br></br>
-                    One of the many reasons I love what I do is that I find it amazing that
-                    a piece of software I helped build has the potential to reach the hands of people all around the world.
+                    I work mostly with the programming languages C# and JavaScript/TypeScript.
+                    ASP.NET MVC is the main framework I use for web applications,
+                    but I'm always open to adopting other technologies.
+                    <br></br>
+                    <br></br>
+                    I've always enjoyed developing different sorts of programs,
+                    not just web-based ones. I began programming in middle school, developing Python applications.
+                    One of the reasons I enjoy programming is knowing that a piece
+                    of software I helped create has the potential to reach the hands of individuals all around the world.
+
 
                 </div>
 
--- components/Intro.tsx
+++ components/Intro.tsx
@@ -12,7 +12,7 @@ const Intro = () => {
                 <div className="flex justify-start mb-6">
 
                     {/* Image Place */}
-                    <div className='shadow-xl shadow-black bg-gradient-to-br from-indigo-200 to-transparent w-16 h-16 rounded-full justify-center flex items-center mr-4'>
+                    <div className='shadow-xl shadow-black bg-gradient-to-br from-indigo-100 to-transparent w-16 h-16 rounded-full justify-center flex items-center mr-4'>
 
                         <img className="w-14 h-14 rounded-full" src="https://media.licdn.com/dms/image/D5603AQGa7Dgb4YwKjg/profile-displayphoto-shrink_100_100/0/1672078313516?e=1677715200&v=beta&t=5yZlaL3lt91uOguhmCYxN307aGsCUOkmkUwxVDPcFWk"></img>
 
